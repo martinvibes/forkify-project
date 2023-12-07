91c7f523e5668dadd42bc33bf5997639003a1367
martin machiebe <martinmachiebe21@gmail.com>
2023-12-06 13:49:52 -1100

    initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b5d7f08f-ff98-47dc-8b43-74e7a396bd5a';
+export const  MODAL_CLOSE_SEC = 2.5;
---
+++ starter/src/js/views/resultview.js
@@ -0,0 +1,15 @@
+import View from "./view.js";
+import previewView from "./previewView.js";
+import icons from "url:../../img/icons.svg";
+
+class ResultView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'no recipes found for your query! please try again';
+  _message = '';
+
+  _generateMarkup() {
+    return this._data.map(result => previewView.render(result, false)).join('');
+  }
+}
+
+export default new ResultView();
